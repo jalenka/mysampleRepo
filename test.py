a0f173d6ba996dd4ef94fe6f532b78592e847c90
Jalen He <jalen@kavout.co>
2018-07-30 01:07:56 -0700

    test merge

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 
+
 def testApp(a):
     a=a+2
     return a
@@ -6,3 +7,11 @@ a=1
 a=a+1
 a=testApp(5)
 print(a)
+
+#%%
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
